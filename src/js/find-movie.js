e0d285cc3d0b39811ca5a0ad54500e5068967f7d
AShvab <shvabovska@gmail.com>
2023-04-22 00:43:58 +0300

    styles changed

--- src/js/find-movie.js
+++ src/js/find-movie.js
@@ -3,6 +3,8 @@
 // const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'+API_KEY;
 // const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 // const searchURL = BASE_URL + '/search/movie?'+API_KEY;
+import {genres} from './genres.js';
+
 const API_KEY = 'api_key=a3ba6241cf3e8caa1456599894b81cc2';
 const BASE_URL = 'https://api.themoviedb.org/3';
 const API_URL = `${BASE_URL}/discover/movie?sort_by=popularity.desc&${API_KEY}`;
@@ -14,84 +16,7 @@ const form = document.getElementById('form');
 const search = document.getElementById('search');
 const tagsEl = document.getElementById('tags');
 
-const genres = [
-      {
-        "id": 28,
-        "name": "Action"
-      },
-      {
-        "id": 12,
-        "name": "Adventure"
-      },
-      {
-        "id": 16,
-        "name": "Animation"
-      },
-      {
-        "id": 35,
-        "name": "Comedy"
-      },
-      {
-        "id": 80,
-        "name": "Crime"
-      },
-      {
-        "id": 99,
-        "name": "Documentary"
-      },
-      {
-        "id": 18,
-        "name": "Drama"
-      },
-      {
-        "id": 10751,
-        "name": "Family"
-      },
-      {
-        "id": 14,
-        "name": "Fantasy"
-      },
-      {
-        "id": 36,
-        "name": "History"
-      },
-      {
-        "id": 27,
-        "name": "Horror"
-      },
-      {
-        "id": 10402,
-        "name": "Music"
-      },
-      {
-        "id": 9648,
-        "name": "Mystery"
-      },
-      {
-        "id": 10749,
-        "name": "Romance"
-      },
-      {
-        "id": 878,
-        "name": "Science Fiction"
-      },
-      {
-        "id": 10770,
-        "name": "TV Movie"
-      },
-      {
-        "id": 53,
-        "name": "Thriller"
-      },
-      {
-        "id": 10752,
-        "name": "War"
-      },
-      {
-        "id": 37,
-        "name": "Western"
-      }
-    ];
+
 
 let selectedGenre = [];
 setGenre();
